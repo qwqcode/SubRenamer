cb53d75226b14aadf5ef9fd9676ed964d01b2fd8
qwqcode <qwqcode@gmail.com>
2024-04-06 23:44:03 +0800

    fix(ci): macos build min-version issue

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 
-APP_NAME="SubRenamer"
-CS_PROJ="../SubRenamer/SubRenamer.csproj"
-
-mkdir -p dist/$APP_NAME
-dotnet publish $CS_PROJ -c Release -r linux-x64 -o dist/$APP_NAME -p:PublishAot=true -p:PublishTrimmed=true -p:TrimMode=link --self-contained
+mkdir -p dist/SubRenamer
+dotnet publish ../SubRenamer/SubRenamer.csproj -c Release -r linux-x64 -o dist/SubRenamer -p:PublishAot=true -p:PublishTrimmed=true -p:TrimMode=link --self-contained
 
 if command -v upx &> /dev/null; then
-  upx -fq dist/$APP_NAME/$APP_NAME dist/$APP_NAME/*.so
+  upx -fq dist/SubRenamer/SubRenamer dist/SubRenamer/*.so
 fi
 
-chmod u+x dist/$APP_NAME/$APP_NAME
-cp resources/App.desktop.template dist/$APP_NAME/$APP_NAME.desktop.template
-cp resources/App.icns dist/$APP_NAME/$APP_NAME.icns
+chmod u+x dist/SubRenamer/SubRenamer
+cp resources/App.desktop.template dist/SubRenamer/SubRenamer.desktop.template
+cp resources/App.icns dist/SubRenamer/SubRenamer.icns
 
 cd dist
-tar -zcvf ${APP_NAME}_linux_amd64.tar.gz --exclude="*/*.dbg" $APP_NAME
+tar -zcvf SubRenamer_linux_amd64.tar.gz --exclude="*/*.dbg" SubRenamer
--- scripts/build-macos.sh
+++ scripts/build-macos.sh
@@ -1,24 +1,23 @@
 #!/bin/sh
 
-APP_NAME="SubRenamer"
-APP_DIR="dist/SubRenamer.app"
 CS_PROJ="../SubRenamer/SubRenamer.csproj"
 
-rm -rf $APP_DIR
+rm -rf dist/SubRenamer.app
 
-mkdir -p $APP_DIR/Contents/Resources
-cp resources/App.plist $APP_DIR/Contents/Info.plist
-cp resources/App.icns $APP_DIR/Contents/Resources/App.icns
+mkdir -p dist/SubRenamer.app/Contents/Resources
+cp resources/App.plist dist/SubRenamer.app/Contents/Info.plist
+cp resources/App.icns dist/SubRenamer.app/Contents/Resources/App.icns
 
-mkdir -p $APP_DIR/Contents/MacOS
-dotnet publish $CS_PROJ -c Release -r osx-arm64 -o $APP_DIR/Contents/MacOS -p:PublishAot=true -p:PublishTrimmed=true -p:TrimMode=link --self-contained
+mkdir -p dist/SubRenamer.app/Contents/MacOS
+dotnet publish $CS_PROJ -c Release -r osx-arm64 -o dist/SubRenamer.app/Contents/MacOS -p:PublishAot=true -p:PublishTrimmed=true -p:TrimMode=link --self-contained
 cd dist
-zip ${APP_NAME}_macos_arm64.zip -9 -r ${APP_NAME}.app -x "*/*\.dsym/*"
+zip SubRenamer_macos_arm64.zip -9 -r SubRenamer.app -x "*/*\.dsym/*"
 cd ..
 
-rm -rf $APP_DIR/Contents/MacOS
+rm -rf dist/SubRenamer.app/Contents/MacOS
 
-mkdir -p $APP_DIR/Contents/MacOS
-dotnet publish $CS_PROJ -c Release -r osx-x64 -o $APP_DIR/Contents/MacOS -p:PublishAot=true -p:PublishTrimmed=true -p:TrimMode=link --self-contained
-zip ${APP_NAME}_macos_amd64.zip -r ${APP_DIR}.app -x "*/*\.dsym/*"
+mkdir -p dist/SubRenamer.app/Contents/MacOS
+dotnet publish $CS_PROJ -c Release -r osx-x64 -o dist/SubRenamer.app/Contents/MacOS -p:PublishAot=true -p:PublishTrimmed=true -p:TrimMode=link --self-contained
+cd dist
+zip SubRenamer_macos_amd64.zip -9 -r SubRenamer.app -x "*/*\.dsym/*"
 cd ..
