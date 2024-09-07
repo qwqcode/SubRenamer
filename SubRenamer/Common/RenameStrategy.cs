938521c50ff77a24237ada7a98a174123c6229f2
qwqcode <qwqcode@gmail.com>
2024-09-07 16:32:42 +0800

    perf: improve renaming strategy

--- SubRenamer/Common/RenameStrategy.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SubRenamer.Common;
-public enum RenameStrategy
-{
-    Copy,       // Copy the renamed subtitle file to the folder where the video is located
-    Directly    // Directly modify the file name of the selected subtitle file
-}
